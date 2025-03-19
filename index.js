6a93d8a9eb36845b736b9bf99ab639273a35185b
jcoleman7 <jcjr758@gmail.com>
2025-03-18 23:47:28 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementById("imgBox");
+        let qrImage = document.getElementById("qrImage");
+        let qrText = document.getElementById("qrText");
+
+        function generateQr() {
+            if (qrText.value.trim() === "") {
+                alert("Please enter a text or URL to generate a QR code.");
+                return;
+            }
+            qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+            imgBox.classList.add("show-img");
+        }
